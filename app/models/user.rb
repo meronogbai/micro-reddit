b03f37e4809dfa5c218870957c92bd49da2b1b3e
Jyoti <jyotikiran12@gmail.com>
2020-10-22 18:48:34 +0530

    Add Post model and validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { maximum: 10 }
   validates :password, presence: true, length: { minimum: 8 }
   validates :email, presence: true, uniqueness: true
+
+  has_many :posts
 end
